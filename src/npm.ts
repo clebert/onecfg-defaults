1e611ffd2ad81a51186f5cb02a375dbdf1426c94
Clemens Akens <clebert@me.com>
2022-02-20 13:24:39 +0100

    Ignore node_modules

--- src/npm.ts
+++ src/npm.ts
@@ -1,10 +1,13 @@
 import type {FileChange} from '@onecfg/core';
 import {mergeContent} from '@onecfg/utils';
+import {gitIgnoreFile} from './git';
 import {vscodeExtensionsFile, vscodeSettingsFile} from './vscode';
 
 /** https://www.npmjs.com */
 export function npm(): readonly FileChange<any>[] {
   return [
+    mergeContent(gitIgnoreFile, [`node_modules`]),
+
     mergeContent(vscodeExtensionsFile, {
       recommendations: [`eg2.vscode-npm-script`],
     }),
